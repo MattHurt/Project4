d962fd29229342a3e10296b8bd1a1d1a2a11339d
MattHurt <48461556+MattHurt@users.noreply.github.com>
2019-03-25 18:27:24 -0600

    Update to LinkedList
    Changed how the add method consider 2 situations.  1: if the chain is empty, new node is loaded into head and tail. 2: if the position changes, the node is loaded at the chains beginning.

--- LinkedList.java
+++ LinkedList.java
@@ -10,21 +10,45 @@ Word: Pandemonium: Wild and noisy disorder, uproar and confusion
 
 public classs LinkedList<T> implements MyCollectionInterfaceProject04<T>
 {
-      private Node firstNode;      // first node in the linked list
+      private <T> Node head;       // first node in the linked list
+      private <T> Node tail;       // last node in the list
    	private int numberOfEntries; // Total count of entries in linked list
+      
+      public LinkedList () {  // clears the list
+         clear();
+      } // end default constructor
+      
+      public void clear () {  // sets head and tail to null to clear list
+         head = null;         // not pointing to anything
+         tail = null;         // not pointing to anything
+         numberOfEntries = 0; // 
+      } // end clear
      
       // adds a new entry to this collection
-   	public boolean add(T newItem) {
-   		boolean result = false;
-   		Node newNode = new Node(newItem);
-   		newNode.next = firstNode;
-   		firstNode = newNode;
+   	public boolean add(T newPosition, T newEntry) {
+   		//boolean result = false;
+   		boolean isSuccessful = true;
+         //Node newNode = new Node(newItem);
+   		//newNode.next = firstNode;
+   		//firstNode = newNode;
    		
-   		if (firstNode.equals(newNode)) {
-   			result = true;
-   			numberOfEntries ++;  
+   		//if (firstNode.equals(newNode)) {
+         if ((newPosition >= 1) && (newPosition <= numberOfEntries + 1)) {
+            Node newNode = new Node(newEntry);
+               if (isEmpty()) {  // Considers if chain is empty
+                  head = newNode;
+                  tail = newNode;
+               }
+               else if (newPosition == 1){   // Considers adding a node at the chain's beginning
+                  newNode.setNextNode(head);
+                  head = newNode;
+               }
+         
+         }
+   			//result = true;
+   			//numberOfEntries ++;  
    		}
    
-   		return result;
+   		//return result;
    	}
 }
